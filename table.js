661ff8bd972f690359ebf3d07a9a8d499ebd282c
Asad Asad <43581992+AsadThafer@users.noreply.github.com>
2022-08-27 20:12:32 +0300

    searchtable fixed

--- table.js
+++ table.js
@@ -25,3 +25,25 @@ $.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
 ).catch((e) =>{
     console.log(e)
 })
+
+function searchtable() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("CryptoSearch");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("CryptoTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
